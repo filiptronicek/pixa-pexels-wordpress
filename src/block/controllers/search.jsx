87bc581763f06edbb39b05a56dc6f8d3e20542bc
Filip Troníček <filip.tronicek@seznam.cz>
2021-02-13 15:02:40 +0000

    Update upon provider deselection

--- src/block/controllers/search.jsx
+++ src/block/controllers/search.jsx
@@ -38,7 +38,10 @@ class SearchController  {
         } else {
             selectedProviderNames.push(provider.name)
         }
-        this.setState({selectedProviderNames: selectedProviderNames})
+		this.setState({selectedProviderNames: selectedProviderNames})
+		const query = this.state.query;
+		this.setState({query: `${query} ` })
+		this.setState({query: query})
     }
 
     setQuery (query) {
